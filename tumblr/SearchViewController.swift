7082c03c25bce5dc43a19d2e553e040c4849fbdb
seejayne <jaynev@gmail.com>
2014-10-12 15:15:03 -0700

    loading animation

--- tumblr/SearchViewController.swift
+++ tumblr/SearchViewController.swift
@@ -10,26 +10,39 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var searchFeedView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        var images = UIImage.animatedImageNamed("loading-", duration: 1)
+        imageView.image = images
+        
+        searchFeedView.alpha = 0
+        
+        delay(2, closure: { () -> () in
+            
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                self.searchFeedView.alpha = 1
+            })
+            
+        })
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
     }
-    */
 
+    
+    
+    
+
+    
 }
